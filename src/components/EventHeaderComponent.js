d79de278d1fc07268daecf43d09b5ac3e8b5ccfa
Marcus Koh <kohchihao@gmail.com>
2018-12-25 22:54:49 +0800

    EventHeaderComponent.js: Update ui

--- src/components/EventHeaderComponent.js
+++ src/components/EventHeaderComponent.js
@@ -1,23 +1,49 @@
-import React from 'react'
-import { primary } from '../utils/Colors';
-const EventHeaderComponent = ({ siteTitle, siteSecondaryTitle }) => (
-  <div
-    style={{
-      background: primary,
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0, color: 'white' }}>{siteTitle}</h1>
-      <p style={{ margin: 0, color: 'white' }}>{siteSecondaryTitle}</p>
-    </div>
-  </div>
-)
+import React, { Component } from 'react'
+import { primary } from '../utils/Colors'
 
-export default EventHeaderComponent
+import { withStyles } from '@material-ui/core/styles'
+import Typography from '@material-ui/core/Typography'
+
+const styles = theme => ({
+  header: {
+    background: primary,
+    marginBottom: '1.45rem',
+    height: '30vh',
+  },
+  container: {
+    margin: '0 auto',
+    padding: '1.45rem 1.0875rem',
+    maxWidth: 960,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    height: '30vh',
+  },
+  whiteText: {
+    color: 'white',
+  }
+})
+
+class EventHeaderComponent extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    const { siteTitle, siteSecondaryTitle } = this.props
+    const { classes } = this.props
+
+    return (
+      <div className={classes.header}>
+        <div className={classes.container}>
+          <Typography variant="h3" className={classes.whiteText}>
+            {siteTitle}
+          </Typography>
+
+          <Typography variant="body1" className={classes.whiteText}>{siteSecondaryTitle}</Typography>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default withStyles(styles)(EventHeaderComponent)
