2b72bac8799b38abe1eb2f4e474f2d3b83dc4fe2
Marcus Koh <kohchihao@gmail.com>
2018-12-25 16:10:38 +0800

    image.js: Add required prop-types

--- src/components/image.js
+++ src/components/image.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { StaticQuery, graphql } from 'gatsby'
 import Img from 'gatsby-image'
+import PropTypes from 'prop-types';
 
 /**
  * Generates an image with lazy loading. The images would automatically optimised. 
@@ -43,4 +44,9 @@ const Image = (props) => (
     }}
   />
 )
+
+Image.propTypes = {
+  filename: PropTypes.string.isRequired,
+}
+
 export default Image
