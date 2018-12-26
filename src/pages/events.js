048d1ac71bfc9c5dc65a66d093bfbb444b9375d8
Marcus Koh <kohchihao@gmail.com>
2018-12-26 12:23:57 +0800

    events.js: Use graphql instead of json

--- src/pages/events.js
+++ src/pages/events.js
@@ -5,7 +5,6 @@ import { withStyles } from '@material-ui/core/styles'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 
-import events from '../data/events.json'
 import EventCardComponent from '../components/EventCardComponent'
 import EventHeaderComponent from '../components/EventHeaderComponent'
 
@@ -25,19 +24,22 @@ const styles = theme => ({
 
 class EventsPage extends Component {
   constructor(props) {
-    super(props);
+    super(props)
   }
 
-  eventsGrid = classes => {
+  eventsGrid = (classes, data) => {
+    const events = data.events.edges;
+    console.log('events',events);
     return (
       <div className={classes.root}>
         <Grid container spacing={24}>
           {events.map((event, index) => (
             <EventCardComponent
-              name={event.name}
-              location={event.location}
-              description={event.description}
-              cover_img={event.cover_img}
+              to={event.node.fields.slug}
+              title={event.node.frontmatter.title}
+              location={event.node.frontmatter.location}
+              description={event.node.frontmatter.description}
+              cover_img={event.node.frontmatter.cover_img.relativePath}
             />
           ))}
         </Grid>
@@ -46,16 +48,44 @@ class EventsPage extends Component {
   }
 
   render() {
-    const { classes } = this.props;
-
+    const { classes } = this.props
+    const { data } = this.props
     return (
       <Layout>
         <SEO title="Events" />
-        <EventHeaderComponent siteTitle={'Events'} siteSecondaryTitle={'Past and present events'} />
-        {this.eventsGrid(classes)}
+        <EventHeaderComponent
+          siteTitle={'Events'}
+          siteSecondaryTitle={'Past and present events'}
+        />
+        {this.eventsGrid(classes, data)}
       </Layout>
     )
   }
 }
 
 export default withStyles(styles)(EventsPage)
+
+export const pageQuery = graphql`
+  query HomeQuery {
+    events: allMarkdownRemark(
+      sort: { fields: [frontmatter___date], order: DESC }
+    ) {
+      edges {
+        node {
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "DD.MM.YYYY")
+            title
+            description
+            location
+            cover_img {
+              relativePath
+            }
+          }
+        }
+      }
+    }
+  }
+`
