695bae7a70b49aaaa0cdb9cd9c346544c4cc7c59
Marcus Koh <kohchihao@gmail.com>
2018-12-26 16:27:37 +0800

    Add ImageCardComponent.js and ImageHeaderComponent.js

---
+++ src/components/ImageHeaderComponent.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import { primary } from '../utils/Colors';
+import PropTypes from 'prop-types';
+
+import { withStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+
+const styles = theme => ({
+  header: {
+    background: primary,
+    marginBottom: '1.45rem',
+    height: '30vh',
+  },
+  container: {
+    margin: '0 auto',
+    padding: '1.45rem 1.0875rem',
+    maxWidth: 960,
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+    height: '30vh',
+  },
+  whiteText: {
+    color: 'white',
+  },
+});
+
+class ImageHeaderComponent extends Component {
+  
+  render() {
+    const { siteTitle, location, siteDescription, date } = this.props;
+    const { classes } = this.props;
+
+    return (
+      <div className={classes.header}>
+        <div className={classes.container}>
+          <Typography variant="h3" className={classes.whiteText}>
+            {siteTitle}
+          </Typography>
+
+          <Typography variant="h6" className={classes.whiteText}>
+            {location} ∙ {date}
+          </Typography>
+
+          <Typography variant="body1" className={classes.whiteText}>
+            {siteDescription}
+          </Typography>
+        </div>
+      </div>
+    );
+  }
+}
+
+ImageHeaderComponent.propTypes = {
+  siteTitle: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
+  siteDescription: PropTypes.string.isRequired,
+};
+
+export default withStyles(styles)(ImageHeaderComponent);
