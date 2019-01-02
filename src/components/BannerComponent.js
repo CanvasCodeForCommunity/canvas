845d03f22eee4644b4e87f90b4898abd18836462
Jin Ying <developer.tjy@gmail.com>
2019-01-02 13:55:43 +0800

    Fix warning massages

--- src/components/BannerComponent.js
+++ src/components/BannerComponent.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import BannerImage from './BannerImage';
 
 import { primary, hoverPrimary } from '../utils/Colors';
-import CoverImage from '../images/home_cover.jpg'
 
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
