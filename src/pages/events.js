64cbbeabfc98b638cdae953606e08d73efb7dd1f
Jin Ying <developer.tjy@gmail.com>
2019-01-06 12:46:09 +0800

    events.js: Add filter to only get events from events folder

--- src/pages/events.js
+++ src/pages/events.js
@@ -69,6 +69,9 @@ export const pageQuery = graphql`
   query HomeQuery {
     events: allMarkdownRemark(
       sort: { fields: [frontmatter___date], order: DESC }
+      filter: {
+        fileAbsolutePath: {regex: "//events/"}
+      }
     ) {
       edges {
         node {
