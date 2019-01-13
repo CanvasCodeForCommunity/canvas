316e1a9da44c1bd9dce6bc33567f901b1a0e1239
DanKhoo <danielkhoo94@hotmail.com>
2019-01-13 20:26:17 +0800

    Add PartnersComponent

---
+++ src/components/home/partners/LogoComponent.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+const styles = theme => ({
+  root: {
+    marginBottom: 15
+  },
+  logo: {
+    position: 'relative',
+    width: '100%'
+  }
+})
+
+class LogoComponent extends Component {
+  render() {
+    const { Logo } = this.props
+    const { classes } = this.props;
+
+    return (
+      <Grid
+        item
+        xs={5}
+        sm={5}
+        className={classes.root}
+      >
+        <div className={classes.logo}>
+          {Logo}
+        </div>
+      </Grid>
+    )
+  }
+}
+
+export default withStyles(styles)(LogoComponent);
---
+++ src/components/home/partners/PartnersComponent.js
@@ -0,0 +1,61 @@
+import React, { Component } from 'react';
+import Image from '../../image'
+
+import LogoComponent from './LogoComponent';
+
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+const styles = theme => ({
+  root: {
+    backgroundColor: 'white',
+    padding: '3rem 1.5rem 3rem 1.5rem',
+    textAlign: 'center',
+  },
+  title: {
+    color: '#000',
+    fontSize: '1.75rem',
+    fontWeight: 700,
+    marginBottom: '1.25em',
+    [theme.breakpoints.up('md')]: {
+      fontSize: '2.125rem',
+      lineHeight: 1.5
+    },
+  },
+  logoImage: {
+    maxHeight: '100%',
+    '& img': {
+      objectFit: 'contain !important'
+    }
+  }
+})
+
+class PartnersComponent extends Component {
+  render() {
+    const { classes } = this.props;
+
+    return (
+      <div className={classes.root}>
+      <Typography variant="h4" className={classes.title}>Awesome Partners</Typography>
+      <Grid container justify="space-around">
+        <LogoComponent
+          Logo={<Image filename={'google.png'} classProps={classes.logoImage} />}
+        />
+        <LogoComponent
+          Logo={<Image filename={'google.png'} classProps={classes.logoImage} />}
+        />
+        <LogoComponent
+          Logo={<Image filename={'google.png'} classProps={classes.logoImage} />}
+        />
+        <LogoComponent
+          Logo={<Image filename={'google.png'} classProps={classes.logoImage} />}
+        />
+      </Grid>
+      </div>
+    )
+  }
+}
+
+
+export default withStyles(styles)(PartnersComponent);
