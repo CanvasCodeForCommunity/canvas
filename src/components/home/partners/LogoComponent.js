f4c61835b85077f2f6b0484a48567a0b868b1556
DanKhoo <danielkhoo94@hotmail.com>
2019-01-17 14:50:37 +0800

    LogoComponent: Format Code

--- src/components/home/partners/LogoComponent.js
+++ src/components/home/partners/LogoComponent.js
@@ -5,31 +5,24 @@ import Grid from '@material-ui/core/Grid';
 
 const styles = theme => ({
   root: {
-    marginBottom: 15
+    marginBottom: 15,
   },
   logo: {
     position: 'relative',
-    width: '100%'
-  }
-})
+    width: '100%',
+  },
+});
 
 class LogoComponent extends Component {
   render() {
-    const { Logo } = this.props
+    const { Logo } = this.props;
     const { classes } = this.props;
 
     return (
-      <Grid
-        item
-        xs={5}
-        sm={5}
-        className={classes.root}
-      >
-        <div className={classes.logo}>
-          {Logo}
-        </div>
+      <Grid item xs={5} sm={5} className={classes.root}>
+        <div className={classes.logo}>{Logo}</div>
       </Grid>
-    )
+    );
   }
 }
 
