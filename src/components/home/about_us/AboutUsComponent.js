dc46086fc05d20b948127ec76a448df858cdb24e
Marcus Koh <kohchihao@gmail.com>
2019-06-02 00:02:15 +0800

    AboutUsComponent.js: Fix styles

--- src/components/home/about_us/AboutUsComponent.js
+++ src/components/home/about_us/AboutUsComponent.js
@@ -17,17 +17,31 @@ const styles = theme => ({
       padding: '3rem 6rem 3rem 6rem', //top right bottom left
     },
   },
+  textGrid: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
   title: {
     color: '#000',
     fontSize: '1rem',
+    
     // fontWeight: 700,
     [theme.breakpoints.up('md')]: {
       fontSize: '1rem',
       lineHeight: 1.5,
     },
   },
+  imageGrid: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center'
+    },
+  },
   iconImage: {
-    maxHeight: '100%',
+    maxHeight: '85%',
     display: 'none',
     [theme.breakpoints.up('md')]: {
       display: 'flex',
@@ -45,7 +59,7 @@ class AboutUsComponent extends Component {
     return (
       <div className={classes.root}>
         <Grid container justify="space-around">
-          <Grid item xs={12} sm={4}>
+          <Grid item xs={12} sm={12} md={4} className={classes.textGrid}>
             <Typography className={classes.title} align="left">
               <Typography variant={'subtitle1'}>
                 <b>#codeForCommunity</b> is here to create learning
@@ -59,7 +73,7 @@ class AboutUsComponent extends Component {
               </Typography>
             </Typography>
           </Grid>
-          <Grid item xs={12} sm={8}>
+          <Grid item xs={12} sm={8} className={classes.imageGrid}>
             <Image filename={'macbookpro.png'} classProps={classes.iconImage} />
           </Grid>
         </Grid>
