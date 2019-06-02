a96affa9db7ba2b607747d6bd6b43476c65a3e43
Jin Ying <developer.tjy@gmail.com>
2019-06-02 14:31:48 +0800

    about.js: Add footer component

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,17 +1,12 @@
 import React, { Component } from 'react';
-import { withStyles } from '@material-ui/core/styles';
+
+import BannerComponent from '../components/about/BannerComponent';
+import TeamComponent from '../components/about/TeamComponent';
+import FooterComponent from '../components/home/footer/FooterComponent';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
-
-import TopComponent from '../components/about/TopComponent';
-import TeamComponent from '../components/about/TeamComponent';
-
 import withRoot from '../components/withRoot';
-
-const styles = theme => ({
-
-})
 
 class AboutPage extends Component {
   render() {
@@ -21,18 +16,19 @@ class AboutPage extends Component {
           title="About"
           keywords={['canvas', 'volunteer', 'singapore', 'computing']}
         />
-        <TopComponent />
+        <BannerComponent />
         <TeamComponent
           advisors={this.props.data.advisors}
           operations={this.props.data.operations}
           partners={this.props.data.partners}
         />
+        <FooterComponent data={this.props.data.site} />
       </Layout>
     );
   }
 }
 
-export default withRoot(withStyles(styles)(AboutPage));
+export default withRoot(AboutPage);
 
 // Team Query
 export const pageQuery = graphql`
