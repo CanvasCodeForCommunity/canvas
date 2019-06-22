9025cdedd9358e0596299fd39ffb143a89d1ec82
Marcus Koh <kohchihao@gmail.com>
2019-06-22 23:48:37 +0800

    Update FooterComponent.js with new color and icons

--- src/components/home/footer/FooterComponent.js
+++ src/components/home/footer/FooterComponent.js
@@ -1,34 +1,59 @@
 import React, { Component } from 'react';
-import { Link } from 'gatsby';
 
 import Typography from '@material-ui/core/Typography';
 import { withStyles } from '@material-ui/core/styles';
 
-import { grey26 } from '../../../utils/Colors';
+import { grey13 } from '../../../utils/Colors';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { primary } from '../../../utils/Colors';
+import {
+  faFacebookF,
+  faTelegramPlane,
+  faInstagram,
+} from '@fortawesome/free-brands-svg-icons/';
+
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 const styles = theme => ({
   root: {
-    backgroundColor: grey26,
-    padding: '2rem 1.5rem 2rem 4rem',
+    backgroundColor: grey13,
+    padding: '2rem 2rem 2rem 2rem', //top right bottom left
+    [theme.breakpoints.up('md')]: {
+      //desktop
+      padding: '5rem 6rem 5rem 6rem',
+    },
   },
   description: {
     marginBottom: 10,
-    color: 'white',
+    color: '#6c757d',
     fontSize: '0.7rem',
     [theme.breakpoints.up('md')]: {
       fontSize: '0.9rem',
     },
+  },
+  descriptionLink: {
+    textDecoration: 'none',
+    color: 'white',
+    fontSize: '0.7rem',
+    '&:hover': {
+      color: primary,
+    },
+    [theme.breakpoints.up('md')]: {
+      fontSize: '0.9rem',
+    },
+    paddingRight: '20px',
   },
   links: {
     textDecoration: 'none',
     color: 'white',
     fontSize: '0.9rem',
     '&:hover': {
-      color: 'rgba(255, 255, 255, 0.8)',
+      color: primary,
     },
     [theme.breakpoints.up('md')]: {
       fontSize: '1.1rem',
     },
+    paddingRight: '20px',
   },
 });
 
@@ -39,23 +64,74 @@ class FooterComponent extends Component {
 
     const buildTime = data.buildTimeZone;
 
+    const ExternalLinkWithIcon = ({ to, className, iconType }) => {
+      return (
+        <a
+          href={to}
+          className={classes.links}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={iconType} />
+        </a>
+      );
+    };
+
+    const ExternalLinkWithText = ({ to, className, text }) => {
+      return (
+        <a
+          href={to}
+          className={className}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {text}
+        </a>
+      );
+    };
+
     return (
       <div className={classes.root}>
-        <Link to={'/facebook'} className={classes.links}>
-          Facebook .{' '}
-        </Link>
-        <Link to={'/instagram'} className={classes.links}>
-          Instagram .{' '}
-        </Link>
-        <Link to={'/twitter'} className={classes.links}>
-          Twitter
-        </Link>
-        <Typography variant="body1" className={classes.description}>
+        <div>
+          <ExternalLinkWithIcon
+            to={'https://www.facebook.com'}
+            className={classes.links}
+            iconType={faFacebookF}
+          />
+
+          <ExternalLinkWithIcon
+            to={'https://www.instagram.com'}
+            className={classes.links}
+            iconType={faInstagram}
+          />
+
+          <ExternalLinkWithIcon
+            to={'https://t.me/CodeForCommunity'}
+            className={classes.links}
+            iconType={faTelegramPlane}
+          />
+
+          <ExternalLinkWithIcon
+            to={'https://www.twitter.com'}
+            className={classes.links}
+            iconType={faEnvelope}
+          />
+        </div>
+
+        <Typography variant="subtitle2" className={classes.description}>
           Updated on {buildTime}
           <br />
-          Designed and built by <u>core team</u>.<br />
+          Designed and built by{' '}
+          <ExternalLinkWithText
+            to={'https://github.com'}
+            className={classes.descriptionLink}
+            text={'core team.'}
+          />
+          <br />
           Copyright &#169; 2019 - Present. All rights reserved.
         </Typography>
+
+        <Typography variant="subtitle2" className={classes.description} />
       </div>
     );
   }
